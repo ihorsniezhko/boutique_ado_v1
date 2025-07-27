abebe3389042f11be2a0f24624b3717267de01ad
ihorsniezhko <ihor.sniezhko@gmail.com>
2025-07-27 13:53:02 +0200

    Add checkout form data caching in payment intent.

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = 'django-insecure-bf_nx7m0sd(kbofoqv1g*ba))$ux)2(ju#mr-kr5#21@5iql_d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+]
 
 
 # Application definition
